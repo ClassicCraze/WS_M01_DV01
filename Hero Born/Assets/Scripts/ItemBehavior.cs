9da27b411e753aaa129f9bfd1332cc65489e16d8
Wade Schrotenboer <schrotew@my.erau.edu>
2025-02-04 18:38:30 -0700

    Added Jump, Shoot, and GUI

--- Hero Born/Assets/Scripts/ItemBehavior.cs
+++ Hero Born/Assets/Scripts/ItemBehavior.cs
@@ -2,19 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
- public class ItemBehavior : MonoBehaviour 
+public class ItemBehavior : MonoBehaviour 
  {
-       // 1
+     // 1
+     public GameBehavior gameManager;
+ 
+     void Start()
+     {
+           // 2                 
+           gameManager = GameObject.Find("GameManager").GetComponent<GameBehavior>();
+     }
+ 
      void OnCollisionEnter(Collision collision)
      {
-         // 2
-         if(collision.gameObject.name == "Player")
+         if (collision.gameObject.name == "Player")
          {
-             // 3
              Destroy(this.transform.parent.gameObject);
-
-             // 4
              Debug.Log("Item collected!");
+ 
+             // 3
+             gameManager.Items += 1;
          }
      }
- }
+ } 
